e2c1df4438464c61538ce37396c099181767fd86
Daniel Ding <41363844+BioniCosmos@users.noreply.github.com>
2026-03-31 02:58:26 +0800

    Implement download-by-URL interface

--- ios/Hentai Downloader/ContentView.swift
+++ ios/Hentai Downloader/ContentView.swift
@@ -1,14 +1,19 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var url = ""
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Form {
+            Section { TextField("URL", text: $url) }
+            Button(action: { print(url) }) {
+                Text("Download").frame(maxWidth: .infinity)
+            }
+            .buttonStyle(.borderedProminent)
+            .controlSize(.large)
+            .clipShape(.capsule)
+            .listRowInsets(.init())
         }
-        .padding()
     }
 }
 
